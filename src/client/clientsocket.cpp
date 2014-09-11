c7244bf634b001ef01bbc4c52c22fa9a7f0a7dbb
Keith Mendoza <pantherse@gmail.com>
2014-09-11 09:25:49 -0700

    Use non-blocking socket in client connection

--- src/client/clientsocket.cpp
+++ src/client/clientsocket.cpp
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <sys/select.h>
 
+#include <common/timeoutexception.h>
 #include <client/clientsocket.h>
 #include <log4cplus/loggingmacros.h>
 using namespace std;
@@ -19,10 +20,15 @@ const bool ClientSocket::initClient(const unsigned &port, const string &server)
         throw invalid_argument("server parameter");
 
     bool retVal = true;
-
+    int err;
     try
     {
         initSocket(port, server);
+    }
+    catch(system_error &e)
+    {
+        LOG4CPLUS_ERROR(logger, "Error encountered connecting to server. Cause: "<<e.what());
+        return false;
     }
     catch(runtime_error &e)
     {
@@ -32,87 +38,64 @@ const bool ClientSocket::initClient(const unsigned &port, const string &server)
 
     do
     {
-        const struct sockaddr_storage &addr = getAddrInfo();
-
-        if(connect(getSocket(), (struct sockaddr *)&addr, getAddrInfoSize()) != 0)
+        try
         {
-            int err = errno;
-            LOG4CPLUS_TRACE(logger, "Value of err: "<<err);
-            switch(err)
+            const struct sockaddr_storage &addr = getAddrInfo();
+            if(connect(getSocket(), (struct sockaddr *)&addr, getAddrInfoSize()) != 0)
             {
-            case ECONNREFUSED:
-            case ETIMEDOUT:
-                LOG4CPLUS_TRACE(logger, "Try next returned IP");
-                try
+                err = errno;
+                if(err == EINPROGRESS)
                 {
-                    initSocket();
+                    waitForWriting(3);
+                    int val;
+                    socklen_t len = sizeof(val);
+                    if((err = getsockopt(getSocket(), SOL_SOCKET, SO_ERROR, &val, &len)))
+                    {
+                        char errmsg[256];
+                        strerror_r(err, errmsg, 256);
+                        LOG4CPLUS_ERROR(logger, "Failed to connect to server. Error message: "<<errmsg);
+                        retVal = false;
+                    }
+                    else
+                        LOG4CPLUS_TRACE(logger, "Connected to server");
+                    break;
                 }
-                catch(runtime_error &e)
+                else
                 {
-                    LOG4CPLUS_ERROR(logger, "Tried all resolved IP addresses.");
+                    char errmsg[256];
+                    strerror_r(err, errmsg, 256);
+                    LOG4CPLUS_ERROR(logger, "Failed to connect to server. Error message: "<<errmsg);
                     retVal = false;
+                    break;
                 }
-                break;
-            default:
-                char errmsg[256];
-                strerror_r(err, errmsg, 256);
-                LOG4CPLUS_ERROR(logger, "Failed to connect to server. Error message: "<<errmsg);
-                retVal = false;
+            }
+            else
+            {
+                LOG4CPLUS_DEBUG(logger, "Connected to server");
                 break;
             }
         }
-        else
+        catch(system_error &e)
         {
-            LOG4CPLUS_DEBUG(logger, "Connected to server");
+            LOG4CPLUS_ERROR(logger, "Error encountered connecting to server. Cause: "<<e.what());
+            retVal = false;
             break;
         }
-    }while(retVal);
-
-    if(retVal)
-    {
-        if(fcntl(getSocket(), F_SETFD, O_NONBLOCK))
+        catch(runtime_error &e)
         {
-            int err = errno;
-            char errmsg[256];
-            strerror_r(err, errmsg, 256);
-            ostringstream msg;
-            msg<<"Failed to set socket to non-blocking. Error message: "<<errmsg;
-            throwSystemError(err, msg.str());
+            LOG4CPLUS_ERROR(logger, "Host/port resolution failed. Cause: " << e.what());
+            retVal = false;
+            break;
         }
-
-        fd_set writefd;
-        FD_ZERO(&writefd);
-        FD_SET(getSocket(), &writefd);
-
-        struct timeval timeout;
-        timeout.tv_sec = 15;
-        timeout.tv_usec = 0;
-
-        switch(select(getSocket()+1, NULL, &writefd, NULL, &timeout))
+        catch(TimeoutException &e)
         {
-            case -1:
-            {
-                int err = errno;
-                char errmsg[256];
-                strerror_r(err, errmsg, 256);
-                LOG4CPLUS_ERROR(logger, "Failed to wait for server connection. Error message: "<<errmsg);
-                retVal = false;
-                break;
-            }
-            case 0:
-            {
-                LOG4CPLUS_ERROR(logger, "Unable to connect to server. Conection timedout");
-                retVal = false;
-                break;
-            }
-            default:
-            {
-                if(FD_ISSET(getSocket(), &writefd))
-                    LOG4CPLUS_DEBUG(logger, "Connection to server ready");
-            }
-        }//switch select()
-    }//wait for connect ready
-    else
+            LOG4CPLUS_ERROR(logger, "Timed-out attempting to connect to server. Try next IP");
+            close(getSocket());
+            initSocket();
+        }
+    }while(1);
+
+    if(!retVal)
         LOG4CPLUS_ERROR(logger, "Unable to connect to the server "<<server<<" on port "<<port);
 
     return retVal;
