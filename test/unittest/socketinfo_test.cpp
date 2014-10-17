10c7b56c89c7edb1289f4b427150827c4a893ec2
Keith Mendoza <pantherse@gmail.com>
2014-10-17 13:33:03 -0700

    Completed SocketInfo parameterized unit test

--- test/unittest/socketinfo_test.cpp
+++ test/unittest/socketinfo_test.cpp
@@ -5,6 +5,24 @@
 namespace cloudmutex
 {
 
+TEST(SocketInfoTest, paramConstructor)
+{
+    ASSERT_THROW(SocketInfo(2, NULL, 0), std::invalid_argument);
+    ASSERT_THROW(SocketInfo(3, NULL, 0), std::invalid_argument);
+
+    struct addrinfo hints;    
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    struct addrinfo *servInfo = nullptr;
+
+    ASSERT_EQ(0, getaddrinfo("localhost", "65535", &hints, &servInfo));
+    ASSERT_NO_THROW(SocketInfo(4, (const struct sockaddr_storage *)servInfo->ai_addr, servInfo->ai_addrlen));
+    freeaddrinfo(servInfo);
+}
+
 TEST(SocketInfoTest, getSocket)
 {
     SocketInfo i;
